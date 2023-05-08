842b82d98f0c001ee7771c1301496d367cf8e021
GUILHERME HENRICK BESSA DE ANDRADE <suelybessa2002@gmail.com>
2023-05-08 00:16:25 -0300

    refactor: Added the software architecture

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CRUD-API
  Create Crud for Server-Side programming material
+# ROTA
+#        Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
+#           GET - Get an information => Pega uma info
+#           POST - Create an information => Cria uma info
+#           PUT - Change all information => Altera toda a info 
+#           PATH - Change part of the information => Altera parte da info
+#           DELETE - Delete an information => Apaga uma info 
+#        
+#       Name - Route identifier => Identificador da rota 
+#
+#  Function (Callbacke) - Responsible for executing some command =>Resposável por executar algum comando
--- index.js
+++ index.js
@@ -1,21 +1,7 @@
+
 const express = require('express')
+const userRoute = require('./src/routes/user.route')
 const app = express()
-/* ROTA
-        Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
-           GET - Get an information => Pega uma info
-           POST - Create an information => Cria uma info
-           PUT - Change all information => Altera toda a info 
-           PATH - Change part of the information => Altera parte da info
-           DELETE - Delete an information => Apaga uma info 
-        
-       Name - Route identifier => Identificador da rota 
-
-        Function (Callbacke) - Responsible for executing some command => Resposável por executar algum comando
-*/
-app.get('/home', (req, res) => {
-  const soma = 1+1
-
-  res.send({soma: soma})
-})
+app.use('/soma', userRoute)
 
 app.listen(5000)
