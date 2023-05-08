919d0c8be483b380ae628d06bf5dcc275fc7d336
GUILHERME HENRICK BESSA DE ANDRADE <suelybessa2002@gmail.com>
2023-05-07 23:46:56 -0300

    Start Services

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
+/* ROTA
+        Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
+           GET - Get an information => Pega uma info
+           POST - Create an information => Cria uma info
+           PUT - Change all information => Altera toda a info 
+           PATH - Change part of the information => Altera parte da info
+           DELETE - Delete an information => Apaga uma info 
+        
+       Name - Route identifier => Identificador da rota 
 
+        Function (Callbacke) - Responsible for executing some command => Resposável por executar algum comando
+*/
 app.get('/home', (req, res) => {
-  res.send('Hello World')
+  const soma = 1+1
+
+  res.send({soma: soma})
 })
 
-app.listen(3000)
+app.listen(5000)
